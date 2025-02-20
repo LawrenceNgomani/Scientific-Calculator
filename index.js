1f60a80576e048ef20c28a7be4170a7d498ee1ac
LawrenceNgomani <144499949+LawrenceNgomani@users.noreply.github.com>
2025-02-20 02:29:48 +0200

    c

---
+++ index.js
@@ -0,0 +1,11 @@
+// My first code
+//console.log('Happy developing ✨')
+
+
+let Name = 'Lawrence';
+let Age = 23;
+let IsApproved = false;
+let FirstName = undefined;
+let SelectColor = null
+
+console.log(name);
